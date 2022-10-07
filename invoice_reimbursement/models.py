db3b7d8c267ea4bedf8385158ed4f6a093657cbe
woodygoodenough <woodygoodenough@outlook.com>
2022-10-07 17:34:11 +0800

    1. add new models for testing

--- invoice_reimbursement/models.py
+++ invoice_reimbursement/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+
+
+class CompanyReim(models.Model):
+    """A company for invoice reimbursement"""
+    company = models.CharField(max_length=30)
+
+
+class ClientReim(models.Model):
+    """A client in a company"""
+    company = models.ForeignKey(CompanyReim, on_delete=models.CASCADE)
+    client = models.CharField(max_length=5)
+    department = models.CharField(max_length=20)
+    position = models.CharField(max_length=20)
+
+
+class FieldReim(models.Model):
+    """A related field of a company"""
+    company = models.ForeignKey(CompanyReim, on_delete=models.CASCADE)
+    field = models.CharField(max_length=40)
