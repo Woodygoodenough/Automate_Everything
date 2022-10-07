dae4da6d4aa99918ba5a43f16d4c55b9c50d18de
woodygoodenough <woodygoodenough@outlook.com>
2022-10-07 20:42:10 +0800

    1. record list from excel

--- invoice_reimbursement/models.py
+++ invoice_reimbursement/models.py
@@ -5,6 +5,10 @@ class CompanyReim(models.Model):
     """A company for invoice reimbursement"""
     company = models.CharField(max_length=30)
 
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.company
+
 
 class ClientReim(models.Model):
     """A client in a company"""
@@ -13,8 +17,16 @@ class ClientReim(models.Model):
     department = models.CharField(max_length=20)
     position = models.CharField(max_length=20)
 
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.client
+
 
 class FieldReim(models.Model):
     """A related field of a company"""
     company = models.ForeignKey(CompanyReim, on_delete=models.CASCADE)
     field = models.CharField(max_length=40)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.field
